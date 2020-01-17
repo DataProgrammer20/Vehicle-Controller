1ff439be05aa255f9a2e591568139c0ea73ce1a5
William <William@BEASTMACHINE>
2020-01-17 11:28:39 -0700

    Initial commit.

---
+++ app/src/main/java/com/william/vehiclecontroller/ui/SelectDeviceActivity.kt
@@ -0,0 +1,13 @@
+package com.william.vehiclecontroller.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.william.vehiclecontroller.R
+
+class SelectDeviceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
